9f4dc4684d82307630719aaed64aec98ba66edd7
Manuel García Solipa <manu@mokalab.es>
2025-03-30 19:48:12 +0200

    feat(header): display country information in header section

--- core/middlewares/with-geoip.ts
+++ core/middlewares/with-geoip.ts
@@ -4,7 +4,7 @@ import { geolocation } from '@vercel/functions';
 export const withGeoip: MiddlewareFactory = (next) => {
   return async (request, event) => {
     const { country } = geolocation(request);
-    request.headers.set('x-country', country ?? 'unknown');
+    request.headers.set('x-country', country ?? 'ES');
 
     return next(request, event);
   };
